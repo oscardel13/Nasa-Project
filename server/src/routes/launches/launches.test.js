29a6c490318962ded59d396eb8dc6a8c1842ef10
Oscar Delgado <oscardel413@gmail.com>
2023-02-15 00:51:46 -0700

    Update launches.test.js

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -8,7 +8,7 @@ const REQ = request(app)
 describe('Launches API', ()=>{
     beforeAll(async()=>{
         await mongoConnect();
-        loadPlanetsData();
+        await loadPlanetsData();
     })
 
     afterAll(async()=>{
